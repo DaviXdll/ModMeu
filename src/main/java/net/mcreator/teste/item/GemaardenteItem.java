e2b9444c9ee1957317fcb987326d256412d6841f
DaviXdll <aa@aa.com>
2021-05-10 17:11:24 -0300

    gema de fogo

---
+++ src/main/java/net/mcreator/teste/item/GemaardenteItem.java
@@ -0,0 +1,68 @@
+
+package net.mcreator.teste.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.teste.procedures.GemaardenteLivingEntityIsHitWithItemProcedure;
+import net.mcreator.teste.TesteModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@TesteModElements.ModElement.Tag
+public class GemaardenteItem extends TesteModElements.ModElement {
+	@ObjectHolder("teste:gemaardente")
+	public static final Item block = null;
+	public GemaardenteItem(TesteModElements instance) {
+		super(instance, 18);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).isImmuneToFire().rarity(Rarity.RARE));
+			setRegistryName("gemaardente");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public boolean hitEntity(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
+			boolean retval = super.hitEntity(itemstack, entity, sourceentity);
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			World world = entity.world;
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				GemaardenteLivingEntityIsHitWithItemProcedure.executeProcedure($_dependencies);
+			}
+			return retval;
+		}
+	}
+}
